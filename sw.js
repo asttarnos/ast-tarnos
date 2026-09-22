9814d2a01b3e7d338b6ec77c23406212712ff05b
asttarnos <bixentesaut@hotmail.com>
2026-09-22 17:05:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@ try{
   });
 }catch(e){ /* config pas encore complétée, on ignore */ }
 
-const CACHE_NAME = 'ast-tarnos-v20';
+const CACHE_NAME = 'ast-tarnos-v22';
 const URLS_TO_CACHE = [
   './',
   './index.html',
